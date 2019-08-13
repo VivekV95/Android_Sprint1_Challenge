fee68e9153dba0860fb03d1305ce075062d6c030
VivekV95 <vivekv210@gmail.com>
2019-08-13 09:44:44 -0500

    Update project

--- SH_SC1/app/src/main/java/com/vivekvishwanath/sh_sc1/EditActivity.kt
+++ SH_SC1/app/src/main/java/com/vivekvishwanath/sh_sc1/EditActivity.kt
@@ -1,12 +1,28 @@
 package com.vivekvishwanath.sh_sc1
 
+import android.app.Activity
+import android.app.ListActivity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vivekvishwanath.sh_sc1.Movie.Companion.MOVIE_KEY
+import kotlinx.android.synthetic.main.activity_edit.*
 
 class EditActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+
+        save_button.setOnClickListener {
+            val movie = Movie()
+            movie.movieTitle = movie_title_text.text.toString()
+            movie.isWatched = watched_switch.isChecked
+            val intent = Intent()
+            intent.putExtra(MOVIE_KEY, movie)
+            setResult(Activity.RESULT_OK)
+            finish()
+        }
     }
 }
+
--- SH_SC1/app/src/main/java/com/vivekvishwanath/sh_sc1/ListActivity.kt
+++ SH_SC1/app/src/main/java/com/vivekvishwanath/sh_sc1/ListActivity.kt
@@ -1,12 +1,27 @@
 package com.vivekvishwanath.sh_sc1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_list.*
 
-class MainActivity : AppCompatActivity() {
+class ListActivity : AppCompatActivity() {
+
+    companion object {
+        const val MOVIE_REQUEST_CODE = 123
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+        add_movie_button.setOnClickListener {
+            val intent = Intent(this, EditActivity::class.java)
+            startActivityForResult(intent, MOVIE_REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        
     }
 }
